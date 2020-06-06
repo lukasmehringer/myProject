e7bece52b5901b078328e89e290fe42eb99e2cc3
lukasmehringer <64968080+lukasmehringer@users.noreply.github.com>
2020-06-06 10:06:39 +0200

    Update add.c

--- add.c
+++ add.c
@@ -1,15 +1,4 @@
 double myAddFcn(double x, double y){
     double z = x + y;
     return z;
-<<<<<<< Updated upstream
 }
-=======
-}
-
-/*
-
-    double x = 0;
-    double y = 5;
-    printf("%lf + %lf = %lf",x,y,myAddFcn(x,y));
-*/
->>>>>>> Stashed changes
